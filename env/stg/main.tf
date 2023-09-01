be8fa599e672c961ede0f777d58cd334ed164a69
hyeop29 <3110ch@naver.com>
2023-09-01 09:17:03 +0900

    Bigbang is back

--- env/stg/main.tf
+++ env/stg/main.tf
@@ -129,14 +129,14 @@ module "bb_bastion" {
 module "bb_rds" {
     source = "../../modules/rds"
     infra_env = local.name
-    private_subnet_ids      = [module.bb_subnet.private_subnet_ids[0], module.bb_subnet.private_subnet_ids[1], module.bb_subnet.private_subnet_ids[2], module.bb_subnet.private_subnet_ids[3]] 
+    private_subnet_ids      = [module.bb_subnet.private_subnet_ids[2], module.bb_subnet.private_subnet_ids[3]] 
     instance_type = "r5"
-    instance_size = "xlarge"
-    cluster_az_list = ["ap-northeast-2a", "ap-northeast-2c", "ap-northeast-2b", "ap-northeast-2d"]
+    instance_size = "large"
+    cluster_az_list = ["ap-northeast-2a", "ap-northeast-2c"]
     db_name = "mydb"
-    cluster_engine_ver = "15.3"
-    cluster_engine = "postgres"
-    cluster_identifier = "postgre-cluster"
+    cluster_engine_ver = "14.6"
+    cluster_engine = "aurora-postgresql"
+    cluster_identifier = "aurora-cluster"
     master_username = "foo"
     skip_final_snapshot = true
     storage_encrypted = true
